7eb91ca183fb7640d0fd9f79ca2f0e11297c9a04
anellysefirme <anellyse.firme@aluno.ce.gov.br>
2026-09-14 14:46:34 -0300

    mao na massa 1

---
+++ 02_conceitos_basicos/Mão na massa1.py
@@ -0,0 +1,11 @@
+
+nome_produto = "sorvete"
+quantidade_estoque = "33"
+preco_unitario ="3.0"
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+print("nome do produto:" , type("sorvete"))
+print("quantidade no estoque:" , type("33"))
+print("preco unitariio:" , type("3.0"))
+print("disponivel para venda" , type(True)) 
