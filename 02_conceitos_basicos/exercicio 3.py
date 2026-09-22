dd1bf50e026df7a145233b4115d3e2abb47d8ad2
anellysefirme <anellyse.firme@aluno.ce.gov.br>
2026-09-22 08:42:28 -0300

    exercicio 3

---
+++ 02_conceitos_basicos/exercicio 3.py
@@ -0,0 +1,16 @@
+"""
+EXERCÍCIO 03: Conta do Nagoya Sushi House
+Disciplina: Lógica de Programação com Python
+
+ENUNCIADO:
+Crie um programa que:
+1. Leia o valor total consumido no restaurante (em R$).
+2. Aplique a taxa de 10% de serviço do garçom.
+3. Exiba o valor final da conta a pagar com mensagem formatada.
+"""
+# TODO: Desenvolva o algoritmo abaixo:
+valor_consumido= float(input("digite o valor total consumido no restaurante (200.0 R$): "))
+taxa_servico= valor_consumido * 0.10
+valor_final= valor_consumido + taxa_servico
+print(f"valor final da conta a pagar: 200.0:R$ {valor_final:.2f}")
+
